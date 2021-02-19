ef17343ded78c54d2d9ce4f507e235c672455bed
thurschm2 <thurschm2@gmail.com>
2021-02-19 10:27:50 +0000

    commit after Heather's first video on React

---
+++ src/App.js
@@ -0,0 +1,44 @@
+import AddTask from './components/AddTask/AddTask';
+import Task from './components/Task/Task';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="header">
+        <h1 className="heading">To Do!</h1>
+        <AddTask/>
+        <p className="outstanding-tasks">You have 3 tasks to complete</p>
+      </header>
+      <main className="all-tasks">
+          <section className="incomplete-tasks">
+              <h2 className="heading">Tasks to do:</h2>
+                <ul className="task-list">
+                  <Task complete={false} />
+                  <Task complete={false}/>
+                  <Task complete={false}/>
+                </ul>
+          </section>
+          <section className="complete-tasks">
+            <h2 className="heading">Complete tasks:</h2>
+            <ul className="task-list">
+                <Task complete={true}/>
+                <Task complete={true}/>
+{/* 
+                <li className="task task--complete">
+                    <p className="task__text">This is a task that needs to be done</p>
+                    <button className="button">Delete</button>
+                </li>
+                <li className="task task--complete">
+                    <p className="task__text">This is a task that needs to be done</p>
+                    <button className="button">Delete</button>
+                </li> */}
+            </ul>
+        </section>
+    </main>
+  </div>
+  );
+    
+}
+
+export default App;
---
+++ src/components/Task/Task.js
@@ -0,0 +1,14 @@
+import './Task.css';
+
+function Task(props) {
+    console.log(props);
+  return (
+    <li className={`task ${props.complete ? 'task--complete': ''}`}>
+        <p className="task__text">This is a task that needs to be done</p>
+        {props.complete === false && <button className="button">Complete</button>}
+        <button className="button">Delete</button>
+    </li>   
+  );
+}
+
+export default Task;
